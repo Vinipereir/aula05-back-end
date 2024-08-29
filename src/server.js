f606b9e9e183548f4bd327e4bcdc57701df56390
Vinipereir <viniciuspereira.tds2.senai@gmail.com>
2024-08-29 10:50:04 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,6 +8,42 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes =[
+    {
+        id : 1,
+        nome: "Nojinho",
+        cor : "Verde"
+    },
+    {
+        id : 2,
+        nome: "Raivoso",
+        cor : "Vermelho"
+    },
+    {
+        id : 3,
+        nome: "Felicidade",
+        cor : "Amarelo"
+    }
+]
+
+const personagens =[
+    {
+        id:100,
+        nome:"Totoro",
+        vivo: true
+    },
+    {
+        id:101,
+        nome:"Bart",
+        vivo: true
+    },
+    {
+        id:102,
+        nome:" Brian O'Conner ",
+        vivo: false
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello , Word"})
 
@@ -17,6 +53,17 @@ app.get("/2tds2", (req, res) => {
 
 })
 
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+
+})
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
+
+})
+
 app.listen(serverPort, () => {
     console.log (`🙌 server satrted on http://localhost${serverPort}`)
 })
+
+
