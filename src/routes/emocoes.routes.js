8cb63d4d7ff3b589fe9d1ccf931235352c067aec
Vinipereir <viniciuspereira.tds2.senai@gmail.com>
2024-09-05 15:40:12 -0300

    feat: create routes configs

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 
 const emocoesRoutes = Router()
 
-const emocoes =[
+let emocoes =[
     {
         id : 1,
         nome: "Nojinho",
@@ -21,19 +21,23 @@ const emocoes =[
 ]
 
 
-app.get("/emocoes", (req, res) => {
+emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes)
 })
 
-emocoes.push(novaEmocao)
-return res.status(200).send(emocoes)
-
-app.post("/emocoes", (req, res) => {
+emocoesRoutes.post("/", (req, res) => {
     const { nome , cor } = req.body
     const novaEmocao = {
         id: emocoes.length + 1,
         nome: nome,
         cor: cor
     }
+
+emocoesRoutes.push(novaEmocao)
+return res.status(201).send(emocoes)
+
+
   
 })
+
+export default emocoesRoutes;
