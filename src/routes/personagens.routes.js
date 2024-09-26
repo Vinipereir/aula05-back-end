674c3be0826fa62cfd8218029ce7c7c86ebb09d3
Vinipereir <158165909+Vinipereir@users.noreply.github.com>
2024-09-26 17:00:24 -0300

    feat: start to create characters routes

--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -1,26 +1,69 @@
 import { Router } from "express"
 
-const emocoesRoutes = Router()
+const personagensRoutes = Router()
 
-const personagens =[
-    {
+let personagens = [   
+    {   
         id:100,
-        nome:"Totoro",
-        vivo: true
+        nome: "Pai do Rocha",
+        studio: "Céu azul",
+        vivo: false,
     },
-    {
+
+    {   
         id:101,
-        nome:"Bart",
+        nome: "Shrek",
+        studio: "Dreamworks",
         vivo: true
+        
     },
-    {
+
+    {   
         id:102,
-        nome:" Brian O'Conner ",
-        vivo: false
+        nome: "Tom Mater",
+        studio: "Pixar",
+        vivo: true
+
     }
 ]
 
-app.get("/personagens", (req, res) => {
-    return res.status(200).send(personagens)
-
+personagensRoutes.get("/",(req, res) => {
+    return res.status(200)
+    .send(personagens)
 })
+
+//Rota para criar um novo personagem
+personagensRoutes.post("/", (req, res) => {
+    const {nome, studio, vivo} = req.body
+    const novoPersonagem = {
+        id: personagens.length + 1,
+        nome: nome,
+        studio: studio,
+        vivo: vivo
+    }
+    personagens.push(novoPersonagem)
+    return res.status(201).send(novoPersonagem )
+});
+
+
+
+personagensRoutes.delete("/:id" , (req,res) => {
+    const {id} = req.params;
+
+    const personagem = personagens.find((person) => person.id == id)
+
+    if (!personagem) {
+        return res.status(404).send({
+            message: "Personagem não encontardo!",
+        });
+    }
+    personagens = personagens.filter((person)=>person.id = id) 
+
+    return res.status(200).send({
+        message: "personagem deletado",
+        personagem,
+    });
+});
+
+
+export default personagensRoutes;
